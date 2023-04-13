86da6397c3138be237fd16495ee4a2fa00df7f37
Kapil1721 <107835784+Kapil1721@users.noreply.github.com>
2023-04-13 11:37:35 +0530

    okay

--- src/section/guestCheckout/orderDetail.js
+++ src/section/guestCheckout/orderDetail.js
@@ -1,13 +1,12 @@
 import { Col, Row } from "antd";
 import React from "react";
-import StyledButton from "../../components/style/button";
 
 const OrderDetail = () => {
   return (
-    <div>
-      <Row justify={"space-around"} align="middle">
+    <div className="gcheckout-main">
+      <Row justify="space-between" align="middle" gutter={[0, 20]}>
         <Col sm={3}>
-          <div className="kamlesh singh">
+          <div className="gcheckout-pimge">
             <img
               src="https://cdn.shopify.com/s/files/1/0618/3183/9957/products/01_63a20998-eb89-46d5-94b7-ac47a2e8a39a_64x64.jpg?v=1680613675"
               alt=""
@@ -15,43 +14,43 @@ const OrderDetail = () => {
           </div>
         </Col>
 
-        <Col sm={14}>
-          <div className="">Cobalt Check</div>
-          <div className="">Navy / XXL / Casual Slim</div>
+        <Col sm={17}>
+          <div className="product-info">Cobalt Check</div>
+          <div className="product-de">Navy / XXL / Casual Slim</div>
         </Col>
 
         <Col sm={3}>
-          <div>₹ 4,490.00</div>
+          <div className="product-info">₹ 4,490.00</div>
         </Col>
 
         <Col xs={24}>
-          <Row>
-            <Col lg={14} xs={15} style={{ marginTop: "7px" }}>
+          <Row justify="space-between">
+            <Col lg={18} xs={15}>
               <input type="text" id="Couponcodes" placeholder="Coupon Code" />
             </Col>
 
-            <Col xxl={8} xl={8} lg={9} md={9} sm={9} xs={9}>
-              <StyledButton sx={{ padding: "13px" }} varinat={"Contained"}>
-                Apply
-              </StyledButton>
+            <Col xxl={5} xl={8} xs={9}>
+              <button>
+                <span>Apply</span>
+              </button>
             </Col>
           </Row>
         </Col>
 
         <Col xs={24}>
           <div className="gcheckout-sum">
-            <div className="">Subtotal</div>
-            <div className="">₹4,490.00</div>
+            <div className="title">Subtotal</div>
+            <div className="amt">₹ 4,490.00</div>
           </div>
 
           <div className="gcheckout-sum">
-            <div className="">Shipping</div>
-            <div className="">Free</div>
+            <div className="title">Shipping</div>
+            <div className="amt">Free</div>
           </div>
 
           <div className="gcheckout-sum">
-            <div className="">Total</div>
-            <div className="">₹4,490.00</div>
+            <div className="title-bld">Total</div>
+            <div className="amt-blder">₹ 4,490.00</div>
           </div>
         </Col>
       </Row>
